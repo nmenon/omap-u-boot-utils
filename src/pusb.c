a0c09d105cfc1baa4064e0f21cb7ca2c91d44cdc
Nishanth Menon <nm@ti.com>
2008-12-03 13:45:17 -0600

    pusb: add example for sudo Add example for using sudo while running pusb
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- src/pusb.c
+++ src/pusb.c
@@ -419,12 +419,14 @@ void help(void)
 	       "   -d device_ID: (optional) USB Device id (Uses default of 0x%X)\n"
 	       "   -f input_file: input file to be transmitted to target\n"
 	       "NOTE: it is required to run this program in sudo mode to get access at times\n"
+	       "Usage Example:\n" "-------------\n"
+	       "sudo %s -f F_NAME \n"
 	       OMAP_UBOOT_UTILS_REVISION
 	       "\nCopyright (C) 2008 Texas Instruments, <www.ti.com>\n"
 	       "This is free software; see the source for copying conditions.\n"
 	       "There is NO warranty; not even for MERCHANTABILITY or FITNESS \n"
 	       "FOR A PARTICULAR PURPOSE.\n",
-	       program_name, search_product);
+	       program_name, search_product, program_name);
 }
 
 int main(int argc, char *argv[])
